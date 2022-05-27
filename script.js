b1cedbebcea13771ba1f48fac339d49e598fa4b0
Nez Mustafa <nezmustafa123@gmail.com>
2022-05-27 20:20:39 +0100

    hide error function

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const btn = document.querySelector(".btn"),
   total = document.querySelector(".total"),
   error = document.querySelector(".error");
 
+const hideError = () => {
+  setTimeout(() => {
+    error.style.display = "none";
+  }, 5000);
+};
 const calculateTip = () => {
   const bill = document.querySelector(".bill").value;
   const rate = document.querySelector(".rate").value;
 
   if (bill === "" || rate === "") {
     console.log("Please add a number");
+
+    error.style.display = "block";
+    hideError();
   }
 };
 btn.addEventListener("click", calculateTip);
